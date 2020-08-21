d94f34184a21cac29c92e3b600501d17d8730da7
Ilya Didenko <didenko.i.a@effective-group.ru>
2020-08-22 00:23:37 +0300

    client-9 Скрипт характеристик и нужд для поселенца

---
+++ Assets/Business simulation/Scripts/PersonalCharacteristic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Business_simulation.Entity;
+using UnityEngine;
+
+public class PersonalCharacteristic : MonoBehaviour
+{
+    /// <summary>
+    /// еда
+    /// </summary>
+    [SerializeField]
+    public Characteristic Food;
+    /// <summary>
+    /// вода
+    /// </summary>
+    [SerializeField]
+    public Characteristic Water;
+    /// <summary>
+    /// сон
+    /// </summary>
+    [SerializeField]
+    public Characteristic Sleep;
+    /// <summary>
+    /// одежда
+    /// </summary>
+    [SerializeField]
+    public Characteristic Clothes;
+    /// <summary>
+    /// развлечение
+    /// </summary>
+    [SerializeField]
+    public Characteristic Entertainment;
+    
+    /// <summary>
+    /// здоровье
+    /// </summary>
+    [SerializeField]
+    public Characteristic Health;
+    /// <summary>
+    /// КПД
+    /// </summary>
+    [SerializeField]
+    public Characteristic Efficiency;
+}
