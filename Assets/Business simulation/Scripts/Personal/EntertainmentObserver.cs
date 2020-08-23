72767608f3545ac326a808c5a6784f24e4760ddb
Ilya Didenko <didenko.i.a@effective-group.ru>
2020-08-24 00:13:11 +0300

    client-24 Скрипт контроля развлечение

---
+++ Assets/Business simulation/Scripts/Personal/EntertainmentObserver.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace BusinessSimulation.Scripts.Personal
+{
+    public class EntertainmentObserver : MonoBehaviour
+    {
+        public float MinEntertainmentPct = 0.2f;
+        public float MinEntertainmentDamageEfficiency = 0.3f;
+        public float DamageEntertainment = 0.01f;
+        
+        private PersonalCharacteristic PersonalCharacteristic;
+        
+        void Start()
+        {
+            PersonalCharacteristic = GetComponent<PersonalCharacteristic>();
+        }
+
+        void Update()
+        {
+            PersonalCharacteristic.Entertainment.Damage(DamageEntertainment * Time.deltaTime);
+            
+            if (PersonalCharacteristic.Entertainment.GetPct() < MinEntertainmentPct)
+            {
+                PersonalCharacteristic.Efficiency.Damage(MinEntertainmentDamageEfficiency * Time.deltaTime);
+            }
+        }
+    }
+}
