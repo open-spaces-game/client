a7823cd7ca3114a75b6166228e585c1b54133339
Ilya Didenko <didenko.i.a@effective-group.ru>
2020-08-25 00:29:00 +0300

    client-14 Скрипт размещения здания [Скрипт строительства здания]

---
+++ Assets/Business simulation/Scripts/Build/TerrainTargetPosition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BusinessSimulation.Scripts.Build
+{
+    public class TerrainTargetPosition : MonoBehaviour
+    {
+        public GameObject Terrain;
+        public float Distance = 1000;
+        public RaycastHit TargetPosition;
+        public bool IsPosition;
+
+        private TerrainCollider TerrainCollider;
+        private RaycastHit Hit;
+        private Ray Ray;
+
+        private void Start()
+        {
+            TerrainCollider = Terrain.GetComponent<TerrainCollider>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            IsPosition = TerrainCollider.Raycast(Ray, out TargetPosition, Distance);
+        }
+    }
+}
