48d8e572fb435663fae717e0c0fe97133bf1097c
Ilya Didenko <didenko.i.a@effective-group.ru>
2020-08-24 23:51:11 +0300

    client-14 Скрипт размещения здания [Добавить скрипт наведения на терейн для размещения здания ]

---
+++ Assets/Business simulation/Scripts/TerrainTargetPosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainTargetPosition : MonoBehaviour
+{
+    public GameObject Terrain;
+    public float Distance = 1000;
+    public RaycastHit TargetPosition;
+    public bool IsPosition;
+    
+    private TerrainCollider TerrainCollider;
+    private RaycastHit Hit;
+    private Ray Ray;
+    
+    private void Start()
+    {
+        TerrainCollider = Terrain.GetComponent<TerrainCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        IsPosition = TerrainCollider.Raycast(Ray, out TargetPosition, Distance);
+    }
+}
