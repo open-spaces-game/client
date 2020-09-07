382fed7a7b0b00fdc089a494b3442fbaa1ff9ae7
Ilya Didenko <didenko.i.a@effective-group.ru>
2020-09-07 23:38:47 +0300

    client-34 При размещении здания не учитываются размещённые строения

--- Assets/Business simulation/Scripts/Build/BuildBlock.cs
+++ Assets/Business simulation/Scripts/Build/BuildBlock.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildBlock : MonoBehaviour
+namespace BusinessSimulation.Scripts.Build
 {
-    public GameObject wallRight;
-    public GameObject wallLeft;
-    public GameObject wallForward;
-    public GameObject wallBack;
+    public class BuildBlock : MonoBehaviour
+    {
+        public GameObject wallRight;
+        public GameObject wallLeft;
+        public GameObject wallForward;
+        public GameObject wallBack;
 
-    public GameObject doorLeft;
-    public GameObject doorRight;
-    public GameObject doorForward;
-    public GameObject doorBack;
+        public GameObject doorLeft;
+        public GameObject doorRight;
+        public GameObject doorForward;
+        public GameObject doorBack;
     }
+}
---
+++ Assets/Business simulation/Scripts/Build/MarkerStructureColor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace BusinessSimulation.Scripts.Build
+{
+    public class MarkerStructureColor : MonoBehaviour
+    {
+        private MeshRenderer[] _meshRenderers;
+
+        private void Start()
+        {
+            _meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        }
+
+        public void ChangeColor(Color color)
+        {
+            foreach (var meshRenderer in _meshRenderers)
+            {
+                meshRenderer.material.color = color;
+            }
+        }
+    }
+}
