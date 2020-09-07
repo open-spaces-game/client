682cf43581cec0e8b158d9d3b19fe55ea8114cf4
Ilya Didenko <didenko.i.a@effective-group.ru>
2020-09-08 00:05:48 +0300

    client-34 При размещении здания не учитываются размещённые строения [колидер со зданием]

--- Assets/Business simulation/Scripts/Build/BuildTarget.cs
+++ Assets/Business simulation/Scripts/Build/BuildTarget.cs
@@ -14,7 +14,7 @@ namespace BusinessSimulation.Scripts.Build
 
         private TerrainTargetPosition TerrainTargetPosition;
         private List<GameObject> buildings;
-        private MarkerStructureValidator markerStructureValidator;
+        private MarkerStructureColliderEnter markerStructureColliderEnter;
         private MarkerStructureColor markerStructureColor;
 
         // Start is called before the first frame update
@@ -22,7 +22,7 @@ namespace BusinessSimulation.Scripts.Build
         {
             TerrainTargetPosition = GetComponent<TerrainTargetPosition>();
             MarkerStructure = Instantiate<GameObject>(TargetStructure);
-            markerStructureValidator = MarkerStructure.AddComponent<MarkerStructureValidator>();
+            markerStructureColliderEnter = MarkerStructure.AddComponent<MarkerStructureColliderEnter>();
             markerStructureColor = MarkerStructure.AddComponent<MarkerStructureColor>();
             buildings = FindBuildings(); 
         }
@@ -35,15 +35,20 @@ namespace BusinessSimulation.Scripts.Build
                 MarkerStructure.SetActive(true);
                 MarkerStructure.transform.position = GetMarkerPosition();
                 
-                var isValidPlace = markerStructureValidator.IsValid(buildings, MarkerStructure);
-                
+                var structure = markerStructureColliderEnter.GetColliderStructure();
+                var isValidPlace = structure == null;
                 var validColor = isValidPlace ? Color.white : Color.red;
                 markerStructureColor.ChangeColor(validColor);
-                
+                // Если есть строение то нужно проверить можно ли к нему присоединить блок
+                // Если нет то создаём новое строение и помещаем блок в нутрь этого строения
                 if (isValidPlace && Input.GetMouseButtonDown(0))
                 {
                     GameObject Structure = Instantiate<GameObject>(TargetStructure);
                     Structure.transform.position = MarkerStructure.transform.position;
+                    var build = new GameObject("build");
+                    
+                    build.transform.position = Structure.transform.position;
+                    Structure.transform.parent = build.transform;
                     buildings.Add(Structure);
                 }
             }
---
+++ Assets/Business simulation/Scripts/Build/MarkerStructureColliderEnter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using BusinessSimulation.Enum;
+using UnityEngine;
+
+namespace BusinessSimulation.Scripts.Build
+{
+    public class MarkerStructureColliderEnter: MonoBehaviour
+    {
+        private bool isValid = true;
+        private string tag;
+        
+        private BuildBlock buildBlock;
+
+        private void Start()
+        {
+            tag = GameTag.building.ToString();
+            
+            foreach (var colliderChild in GetComponentsInChildren<Collider>())
+            {
+                colliderChild.isTrigger = true;
+            }
+            
+            gameObject.AddComponent<Rigidbody>();
+        }
+
+        public BuildBlock GetColliderStructure()
+        {
+            return buildBlock;
+        }
+
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.transform.parent && other.transform.parent.CompareTag(tag))
+            {
+                buildBlock = other.transform.parent.GetComponent<BuildBlock>();
+                isValid = false;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.transform.parent && other.transform.parent.CompareTag(tag))
+            {
+                buildBlock = null;
+            }
+        }
+    }
+}
